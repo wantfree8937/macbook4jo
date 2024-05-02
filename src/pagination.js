79e09c7ab10714b7c4e92294e74dc9c270469b74
ovew <ovewknds@naver.com>
2024-05-03 04:29:41 +0900

    1st draft Completion before Build

--- src/pagination.js
+++ src/pagination.js
@@ -1,21 +1,26 @@
-let totalPostCount = 0;
+let totalDataCount = 0; //총 데이터의 갯수
+let dataCount = 9; //한 페이지에 나타낼 데이터 갯수
+let currentPage = 0;
+let paginationCount = 10; //화면에 나타낼 페이징 갯수
 
 const pagination = () => {
     //총 게시물 갯수가 9개 이하면 페이징 비활성화 
-    if (totalPostCount <= 9) return;
+    if (totalDataCount <= 9) return;
 
-    let totalPage = Math.ceil(postCount / 20);
+    //총 페이지 갯수 계산. perPage는 한 페이지 당 보여줄 데이터의 갯수
+    let totalPage = Math.ceil(totalDataCount / dataCount);
     //현재 페이지에서 보여지는 페이징 그룹
-    let pageGroup = Math.ceil(currentPage / 10);
+    let currentPagination = Math.ceil(currentPage / paginationCount);
 
-    let lastPage = pageGroup * 10;
-    if (lastPage > totalPage) lastPage = totalPage;
-    let firstPage = lastPage - (10 - 1) <= 0 ? 1 : lastPage - (10 - 1);
+    let lastPageNum = paginationCount * dataCount;
+    if (lastPageNum > totalPage) lastPageNum = totalPage;
+    let firstPageNum = lastPageNum - (paginationCount - 1) <= 0 ? 1 : lastPageNum - (paginationCount - 1);
 
-    const fragmentPage = document.createDocumentFragment();
+    let next = lastPageNum + 1;
+    let prev = firstPageNum - 1;
 
-    if (prev > 0) {
-
+    for (let i = firstPageNum; i <= lastPageNum; i++){
+        html += `<button class='pageNum' id = '${i}page'>${i}</button>`
     }
 
 }
