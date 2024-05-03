e2bc392841703f3ed121b8a92636690bde642078
ovew <ovewknds@naver.com>
2024-05-03 11:38:16 +0900

    Logic edit & Add element

--- src/pagination.js
+++ src/pagination.js
@@ -1,64 +1,29 @@
-import {fetch_MovieData} from './cinescript.js';
-const paginationContainer = document.querySelector('.pagination__container');
-const spanPage = document.querySelectorAll(".current__page");
-const divPage = document.querySelector('btn__page');
-const prevBtn = document.querySelector('btn__prev');
-const nextBtn = document.querySelector('btn__next');
-let totalDataCount = Object.keys(fetch_MovieData.id).length; //총 데이터의 갯수
-let dataCount = 9; //한 페이지에 나타낼 데이터 갯수
-let totalPageCount = 0;
-let currentPage = 1;
-let perPageGroup = 10; //화면에 나타낼 페이징 갯수
-let currentPagination = 0;
-let startPageNum = 0;
-let endPageNum = 0;
+import { fetch_MovieData } from './cinescript.js';
+//총 데이터 개수
+let totalDataCount = Object.keys(fetch_MovieData.id).length;
 
+// 버튼 클릭이벤트
+export const clickBtnEvent = () => {
 
-export const pagination = () => {
-
-    //총 게시물 개수가 9개 이하면 페이징 비활성화 
-    if (totalDataCount <= dataCount ) return;
-
-    //총 페이지 개수 계산. perPage는 한 페이지 당 보여줄 데이터의 갯수
-    totalPageCount = Math.ceil(totalDataCount / dataCount);
-    //현재 페이지에서 보여지는 페이징 그룹
-    currentPagination = Math.ceil(currentPage / perPageGroup);
-
-    endPageNum = perPageGroup * dataCount;
-    if (endPageNum > totalPage) endPageNum = totalPage;
-    startPageNum = endPageNum - (perPageGroup - 1) <= 0 ? 1 : endPageNum - (perPageGroup - 1);
-
-}
-
-//초기화
-export const setPageBtn = () => {
-    paginationContainer.innerHTML = "";
-    for (let i = startPageNum; i <= endPageNum; i++){
-        pageBtn.innerHTML += `<span class='current__page' id='${i}page' >${i}</span>`;
-    }
-}
-
-//현재 페이지버튼 활성화
-export const currentPageActive = () => {
-    divPage.forEach((item) => {
-        let text = item.querySelector(`'#${item}page'`).innerText;
-        if (text.includes(`${item}`)) item.classList.add("active")
-        else item.classList.remove("active");
+    document.querySelector('btn__first').addEventListener('click', () => {
+        changePage("first");
+    });
+    document.querySelector('btn__end').addEventListener('click', () => {
+        changePage("end");
+    });
+    document.querySelector('btn__prev').addEventListener('click', () => {
+        if (currentPage > 1) currentPage -= 1;
+        changePage("prev");
+    });
+    document.querySelector('btn__next').addEventListener('click', () => {
+        if (currentPage < totalPageCount) currentPage += 1;
+        changePage("next");
+    });
+    document.querySelectorAll(".current__page").addEventListener("click", (e) => {
+        console.log(e.target.value);
+        setPagination(e.target.value);
     });
 }
-
-
-export const selectPageBtn = (page) => {
-    page = page == null ? 1 : page;
-    //첫 페이지에서 나타나는 페이징
-    let firstPage = (page - 1) * perPageGroup + 1;
-    //마지막 페이지에서 나타나는 페이징
-    let lastPage = firstPage + perPageGroup - 1;
-    //총 페이지 개수보다 마지막 페이지 번호가 더 크면 총 페이지 개수를 마지막 페이지 번호로
-    lastPage = lastPage > totalPageCount ? totalPageCount : lastPage;
-}
-
-
 
 // 각 페이지버튼 상태변화
 export const changePage = (page) => {
@@ -66,23 +31,52 @@ export const changePage = (page) => {
     let pageStatus = parseInt(document.querySelectorAll(".current__page .active ").innerText);
 
     if (page === "first") page = "1";
-    else if (page === "last") page = totalPageCount;
+    else if (page === "end") page = totalPageCount;
     else if (page === "prev") page = (pageStatus - 1) < 1 ? pageStatus : (pageStatus - 1);
     else if (page === "next") page = (pageStatus + 1) < totalPageCount ? totalPageCount : (pageStatus + 1);
     
-    if (pageStatus != page) selectPage(page);
+    if (pageStatus != page) setPagination(page);
 }
 
-// 버튼 클릭이벤트
-export const clickBtnEvent = () => {
-    document.querySelectorAll(".current__page").addEventListener("click", (e) => {
-        console.log(e.target);
-        selectPage(e.target);
-    });
-    prevBtn.addEventListener('click', () => {
-        if (currentPage > 1) currentPage -= 1;
-    });
-    nextBtn.addEventListener('click', () => {
-        if (currentPage < totalPageCount) currentPage += 1;
-    });
+
+export const setPagination = (page) => {
+
+    let dataCount = 9; //한 페이지에 나타낼 데이터 개수
+    let perPageGroup = 10; //화면에 나타낼 페이징 갯수
+    let firstPage = 1; //페이지가 렌더링되면 첫 페이지부터
+
+    if (page == null || page == 0) page = firstPage;
+    //총 게시물 개수가 9개 이하면 페이징 비활성화 
+    if (totalDataCount <= dataCount ) return;
+    //총 페이지 개수 계산. perPage는 한 페이지 당 보여줄 데이터의 갯수
+    let totalPageCount = Math.ceil(totalDataCount / dataCount);
+    //페이징그룹
+    let paginationGroup = Math.ceil(currentPage / perPageGroup);
+    //현재페이지 튜플의 끝 번호
+    let lastPage = paginationGroup * perPageGroup; 
+    if (lastPage > totalPageCount) lastPage = totalPageCount;
+    //현재페이지 튜플 위치
+    let currentPage = lastPage - 4;
+    //현재페이지 튜플의 시작번호
+    let startPage = lastPage - (perPageGroup - 1) <= 0 ? 1 : lastPage - (perPageGroup - 1);
+
+    initializeBtn(startPage, lastPage);
+
 }
+
+//초기화
+export const initializeBtn = (startPage, lastPage) => {
+    let div = document.querySelector('btn__page').innerHTML = "";
+    for (let i = startPage; i <= lastPage; i++){
+        div.innerHTML += `<span class='${i}page' id='page' >${i}</span>`;
+    }
+}
+
+//현재 페이지버튼 활성화
+export const currentPageActive = () => {
+    document.querySelector('btn__page').forEach((item) => {
+        let text = item.querySelector(`'.${item}page'`).innerText;
+        if (text.includes(`${item}`)) item.classList.add("active")
+        else item.classList.remove("active");
+    });
+};
